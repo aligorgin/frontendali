2bd263dadd57b30312d2f7e6e3de4d4f149b43f1
ali <aliwolfi18@gmail.com>
2021-11-03 17:35:48 +0330

    added favicon

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import App from "../ui/App";
+
+const Home: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>FrontendAli</title>
+                <meta name="description" content="hello,i'm ali a front end dev."/>
+                <link rel="icon" href=""/>
+            </Head>
+            <App/>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/ui/App.tsx
@@ -0,0 +1,8 @@
+export default function App(){
+    return(
+        <div className='bg-red-300 text-lg flex space-x-6'>
+            <div>hey</div>
+            <div>hey</div>
+        </div>
+    )
+}
