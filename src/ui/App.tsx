ce89194f37fe2b50aa466e0a44554a1cc6e7b344
ali <aliwolfi18@gmail.com>
2021-11-03 17:46:34 +0330

    some changes

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,6 @@
 export default function App(){
     return(
-        <div className='bg-red-300 text-lg flex space-x-6'>
+        <div className='bg-red-300 text-lg flex space-x-6 justify-center items-center max-w-2xl mx-auto'>
             <div>hey</div>
             <div>hey</div>
         </div>
