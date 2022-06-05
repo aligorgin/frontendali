cb2775bf1fd27672b6da81677bb189e66a954dce
ali <aliwolfi18@gmail.com>
2022-06-05 21:45:15 +0430

    cards responsived

--- src/ui/components/Cards.tsx
+++ src/ui/components/Cards.tsx
@@ -14,13 +14,13 @@ interface Props {
 
 export default function Cards({children, desc, iconSrc, image}: Props) {
     return (
-        <div className='h-[25rem]'>
-            <div className='h-[20rem] min-w-[30.938rem] rounded-lg flex flex-col shadow-lg'>
+        <div className='mb-20'>
+            <div className='h-[12rem] sm:h-[20rem] min-w-[16rem] sm:min-w-[30.938rem] rounded-lg flex flex-col shadow-lg'>
                 <div>
                     {image === 'weather' && <ImageCards image={'weather'}/>}
                 </div>
 
-                <div className='flex justify-between items-center h-full w-full px-4'>
+                <div className='flex justify-between items-center h-full px-2 sm:px-4 text-sm'>
                     <div>{desc}</div>
                     <div
                         className='cursor-pointer transition hover:-translate-y-1 active:-translate-y-0.5 hover:brightness-125 '>
--- src/ui/containers/Projects.tsx
+++ src/ui/containers/Projects.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
             <h2 className='text-4xl font-semibold  mt-12'>
                 Projects
             </h2>
-            <div className='mt-4 max-w-full relative'>
+            <div className='mt-4 max-w-full relative '>
                 <ScrollContainer vertical={false} horizontal={true} className={'flex w-full space-x-3 relative'}>
                     <Cards desc={'simple app that shows weather'} iconSrc={'https://github.com/aligorgin/weatherS'} image={'weather'} />
                     <Cards desc={'simple app that shows weather'} iconSrc={'https://github.com/aligorgin/weatherS'} image={'weather'} />
