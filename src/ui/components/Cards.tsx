ca50757086b53ee2232665aeb45c0102fe498e88
ali <aliwolfi18@gmail.com>
2022-07-15 21:53:32 +0430

    desc in dark mode

--- src/ui/components/Cards.tsx
+++ src/ui/components/Cards.tsx
@@ -12,15 +12,15 @@ interface Props {
 
 export default function Cards({ desc, iconSrc, image}: Props) {
     return (
-        <div className='mb-20'>
-            <div className='h-[12rem] sm:h-[20rem] min-w-[16rem] sm:min-w-[30.938rem] rounded-lg flex flex-col shadow-lg'>
+        <div className='mb-[3rem]'>
+            <div className='h-[12rem] sm:h-[19rem] min-w-[18rem] sm:min-w-[30.938rem] rounded-lg flex flex-col shadow-lg'>
                 <div>
                     {image === 'outdoor' && <ImageCards image={'outdoor'}/>}
                     {image === 'weather' && <ImageCards image={'weather'}/>}
                     {image === 'twitter' && <ImageCards image={'twitter'}/>}
                 </div>
 
-                <div className='flex justify-between items-center h-full px-2 sm:px-4 text-sm'>
+                <div className='flex justify-between items-center h-full px-2 sm:px-4 text-sm dark:bg-zinc-700 rounded-br-lg rounded-bl-lg'>
                     <div>{desc}</div>
                     <div
                         className='cursor-pointer transition hover:-translate-y-1 active:-translate-y-0.5 hover:brightness-125 '>
