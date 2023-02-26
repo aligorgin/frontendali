a5bfd1b817799f68437ac375a08a128dfe0a1a19
Ali <39724410+aligorgin@users.noreply.github.com>
2023-02-26 10:00:58 +0330

    Update Words.tsx

--- src/ui/contents/Words.tsx
+++ src/ui/contents/Words.tsx
@@ -1,5 +1,5 @@
 const Words={
-    about:'Happy to see you, I\'m Ali Gorgin, a 24-year-old self-taught front-end Developer living in Tehran. I like to build and develop web apps.',
+    about:'Happy to see you. I\'m Ali Gorgin, a 24-year-old self-taught front-end Developer living in Tehran. I like to build and develop web apps.',
 }
 
 export default Words;
