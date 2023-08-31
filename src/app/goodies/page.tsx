d25e10ba934a2e3a9c703f38a4b2c6f07d5410bf
ali <aliigorgin@gmail.com>
2023-08-31 21:37:47 +0330

    added shadow to goodies

--- src/app/goodies/page.tsx
+++ src/app/goodies/page.tsx
@@ -17,19 +17,34 @@ export default function Page() {
 			className="px-4 pt-8 md:px-0"
 		>
 			<div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
-				<div className="relative h-[260px] w-full cursor-pointer rounded-md border border-black/30 dark:border-white/30">
+				<div className="relative h-[260px] w-full cursor-pointer rounded-md border dark:border-white/30">
 					{loaded && <div className="shimmer h-full w-full" />}
 					<Image
 						src="/flipingCard.png"
 						fill
-						sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-						className="rounded-md object-cover"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						className="rounded-md"
 						alt={'image'}
 						priority
 						onLoad={handleImageLoad}
 					/>
+					<div className="absolute bottom-0 w-full rounded-bl-md rounded-br-md bg-white/40 px-4 py-2 shadow-lg backdrop-blur-md dark:shadow-white/20">
+						Fliping Card
+					</div>
 				</div>
 			</div>
+			{/* <div className="max-w-xs overflow-hidden rounded-lg bg-white shadow-md">
+				<Image
+					width={48}
+					height={48}
+					className="h-48 w-full"
+					src={'/flipingCard.png'}
+					alt={'image'}
+				/>
+				<div className="p-4">
+					<h2 className="text-xl font-semibold text-gray-800">fliping card</h2>
+				</div>
+			</div> */}
 		</motion.div>
 	);
 }
