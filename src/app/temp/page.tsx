49befd474ceb48d2e63f2ce785aef315986c2887
ali <aliigorgin@gmail.com>
2023-09-11 16:58:28 +0330

    actions

--- src/app/temp/page.tsx
+++ src/app/temp/page.tsx
@@ -5,11 +5,12 @@ import StaleClosure from '../../containers/practice/states/StaleClosure';
 import Post from '../../containers/practice/states/WrongFetchDataWithUseEffect';
 import Slides from '../../containers/practice/CarouselSlider';
 import Console from '../../containers/practice/Console';
+import TicTacToe from '../../containers/practice/TicTacToe';
 
 export default function Page() {
 	return (
 		<div className="flex min-h-screen items-center justify-center">
-			<Slides />
+			<TicTacToe />
 		</div>
 	);
 }
---
+++ src/containers/practice/TicTacToe.tsx
@@ -0,0 +1,21 @@
+export default function TicTacToe() {
+	return (
+		<div className="flex flex-col border-separate">
+			<div className="flex border-separate">
+				<div className="border border-black px-4 py-2 dark:border-white">X</div>
+				<div className="border border-black px-4 py-2 dark:border-white">X</div>
+				<div className="border border-black px-4 py-2 dark:border-white">X</div>
+			</div>
+			<div className="flex">
+				<div className="border border-black px-4 py-2 dark:border-white">X</div>
+				<div className="border border-black px-4 py-2 dark:border-white">X</div>
+				<div className="border border-black px-4 py-2 dark:border-white">X</div>
+			</div>
+			<div className="flex">
+				<div className="border border-black px-4 py-2 dark:border-white">X</div>
+				<div className="border border-black px-4 py-2 dark:border-white">X</div>
+				<div className="border border-black px-4 py-2 dark:border-white">X</div>
+			</div>
+		</div>
+	);
+}
